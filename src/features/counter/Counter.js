2356468ed43281ae126517982354de3b349d5de9
vivek <vivekrealc2@gmail.com>
2023-08-30 20:23:36 +0530

    calculator just for fun

--- src/features/counter/Counter.js
+++ src/features/counter/Counter.js
@@ -1,66 +1,101 @@
-import React, { useState } from 'react';
-import { useSelector, useDispatch } from 'react-redux';
-import {
-  decrement,
-  increment,
-  incrementByAmount,
-  incrementAsync,
-  incrementIfOdd,
-  selectCount,
-} from './counterSlice';
-import styles from './Counter.module.css';
+import React, { useState } from "react";
+import { useSelector, useDispatch } from "react-redux";
+import { add, subtract, multiply, divide, selectCount } from "./counterSlice";
 
 export function Counter() {
-  const count = useSelector(selectCount);
+  const calValue = useSelector(selectCount);
   const dispatch = useDispatch();
-  const [incrementAmount, setIncrementAmount] = useState('2');
+  const [input1, setInput1] = useState("");
+  const [input2, setInput2] = useState("");
+  const [activeInput, setActiveInput] = useState("input1"); // Track active input
 
-  const incrementValue = Number(incrementAmount) || 0;
+  const handleNumberClick = (num) => {
+    if (activeInput === "input2") {
+      setInput2(input2 + num);
+    } else if (input1 === "" || input2 === "") {
+      setInput1(input1 + num);
+      setInput2("");
+    } else {
+      setInput2(input2 + num);
+    }
+  };
 
   return (
-    <div>
-      <div className={styles.row}>
-        <button
-          className={styles.button}
-          aria-label="Decrement value"
-          onClick={() => dispatch(decrement())}
-        >
-          -
-        </button>
-        <span className={styles.value}>{count}</span>
-        <button
-          className={styles.button}
-          aria-label="Increment value"
-          onClick={() => dispatch(increment())}
-        >
-          +
-        </button>
-      </div>
-      <div className={styles.row}>
-        <input
-          className={styles.textbox}
-          aria-label="Set increment amount"
-          value={incrementAmount}
-          onChange={(e) => setIncrementAmount(e.target.value)}
-        />
-        <button
-          className={styles.button}
-          onClick={() => dispatch(incrementByAmount(incrementValue))}
-        >
-          Add Amount
-        </button>
-        <button
-          className={styles.asyncButton}
-          onClick={() => dispatch(incrementAsync(incrementValue))}
-        >
-          Add Async
-        </button>
-        <button
-          className={styles.button}
-          onClick={() => dispatch(incrementIfOdd(incrementValue))}
-        >
-          Add If Odd
-        </button>
+    <div className="min-h-screen flex items-center justify-center bg-gray-100">
+      <div className="bg-white p-8 rounded shadow-md">
+        <div className="mb-4">
+          <span className="text-2xl font-bold">Result: {calValue}</span>
+        </div>
+        <div className="mb-4 flex space-x-2">
+          <input
+            className="border rounded px-2 py-1 flex-1"
+            placeholder="Enter value"
+            value={input1}
+            onChange={(e) => {
+              setInput1(e.target.value);
+            }}
+            onClick={() => {
+              setActiveInput("input1"); // Set active input to input1 when clicked
+            }}
+          />
+          <input
+            id="input2"
+            className="border rounded px-2 py-1 flex-1"
+            placeholder="Enter value"
+            value={input2}
+            onChange={(e) => {
+              setInput2(e.target.value);
+            }}
+            onClick={() => {
+              setActiveInput("input2"); // Set active input to input2 when clicked
+            }}
+          />
+        </div>
+        <div className="mb-2 grid grid-cols-3 gap-2">
+          {Array.from({ length: 9 }, (_, i) => i + 1).map((num) => (
+            <button
+              key={num}
+              className="bg-blue-500 hover:bg-blue-700 text-white font-bold px-4 py-2 rounded"
+              onClick={() => handleNumberClick(num)}
+            >
+              {num}
+            </button>
+          ))}
+        </div>
+        <div className="grid grid-cols-1 gap-0 px-32">
+          <button
+            className="bg-blue-500 hover:bg-blue-700 text-white font-bold px-4 py-2 rounded col-span-3"
+            onClick={() => handleNumberClick(0)}
+          >
+            0
+          </button>
+        </div>
+        <div className="grid grid-cols-2 gap-2 mb-4 mt-2">
+          <button
+            className="bg-green-500 hover:bg-green-700 text-white font-bold px-4 py-2 rounded col-span-1"
+            onClick={() => dispatch(add({ input1, input2 }))}
+          >
+            Add
+          </button>
+          <button
+            className="bg-red-500 hover:bg-red-700 text-white font-bold px-4 py-2 rounded col-span-1"
+            onClick={() => dispatch(subtract({ input1, input2 }))}
+          >
+            Subtract
+          </button>
+          <button
+            className="bg-yellow-500 hover:bg-yellow-700 text-white font-bold px-4 py-2 rounded col-span-1"
+            onClick={() => dispatch(multiply({ input1, input2 }))}
+          >
+            Multiply
+          </button>
+          <button
+            className="bg-purple-500 hover:bg-purple-700 text-white font-bold px-4 py-2 rounded col-span-1"
+            onClick={() => dispatch(divide({ input1, input2 }))}
+          >
+            Divide
+          </button>
+        </div>
       </div>
     </div>
   );
